b7228326ccf70d8c6153547d22e38be72940fb64
Vivi <karen_vi1996@hotmail.com>
2020-10-29 21:59:56 -0500

    @Vivi, Se realizo el paso nuemero 8 y se agregaron lineas de comando al archivo menu_principal

--- menu_principal.cpp
+++ menu_principal.cpp
@@ -29,5 +29,19 @@ int menu_principal()
 			printf("\nError, introduzca otro valor.\n");
 	}while((opcion<1)||(opcion>3));
 	return(opcion);
+
+	//paso 8 
+	printf ("\t\t\t\t _____                                                 _____ \n");
+    printf ("\t\t\t\t( ___ )-----------------------------------------------( ___ ) \n");
+    printf ("\t\t\t\t |   |                                                 |   | \n"); 
+    printf ("\t\t\t\t |   |             ELIJA EL MODO DE JUEGO              |   | \n");
+    printf ("\t\t\t\t |   |                                                 |   | \n");
+    printf ("\t\t\t\t |   |                    1. VECTOR                    |   | \n");
+    printf ("\t\t\t\t |   |                    2. MATRIZ                    |   | \n");
+    printf ("\t\t\t\t |   |                    3. ARCHIVO                   |   | \n"); 
+	printf ("\t\t\t\t |___|                                                 |___| \n");     
+    printf ("\t\t\t\t(_____)-----------------------------------------------(_____)  \n"); 
+    printf("\n\t\t\t\t\t");
+
 }
 
